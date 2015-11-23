c78620553bc9c4c090a1f6d1cfcc1d811a548954
alizarion <selim.bensenouci@gmail.com>
2015-11-23 20:49:15 +0100

    Update index.php

--- php-app/index.php
+++ php-app/index.php
@@ -1,7 +1,11 @@
-<html>
   <body>
-    <h1>Hello, <?php echo($_SERVER['REMOTE_USER']) ?></h1>
-    <pre><?php print_r(apache_request_headers()); ?></pre>
+    <h1>Hello, <?php echo(apache_request_headers()['OIDC_CLAIM_name']) ?></h1>
+    <pre><?php
+$headers = apache_request_headers();
+foreach ($headers as $header => $value) {
+    echo "$header: $value <br />\n";
+}
+?></pre>
     <a href="/protected/redirect_uri?logout=https%3A%2F%2Flocalhost%2Floggedout.html">Logout</a>
   </body>
 </html>
