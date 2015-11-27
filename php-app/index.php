80ca86fb5d77264185a956282a27770869dae794
alizarion <selim.bensenouci@gmail.com>
2015-11-27 11:26:15 +0100

    Update index.php

--- php-app/index.php
+++ php-app/index.php
@@ -2,209 +2,153 @@
 <header>
 <link href='http://fonts.googleapis.com/css?family=Muli' rel='stylesheet' type='text/css'>
 <style type="text/css" media="screen">
+
+
 body {
-  width: 100%;
-  height: 100%;
-  position: fixed;
-  background-color: #34495e;
+    width: 100%;
+    height: 100%;
+    position: fixed;
+    background-color: #34495e;
 }
 .content {
-  width: 400px;
-  font-size: 36px;
-  line-height: 40px;
-  font-family: 'Muli';
-  color: #ecf0f1;
-  height: 40px;
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  margin-top: -15px;
-  margin-left: -118px;
+    width: 600px;
+    font-size: 36px;
+    line-height: 40px;
+    font-family: 'Muli';
+    color: #ecf0f1;
+    height: 40px;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    margin-top: -15px;
+    margin-left: -118px;
 }
 .content:before {
-  content: '[';
-  position: absolute;
-  left: -30px;
-  line-height: 40px;
+    content: '[';
+    position: absolute;
+    left: -30px;
+    line-height: 40px;
 }
 .content:after {
-  content: ']';
-  position: absolute;
-  right: -30px;
-  line-height: 40px;
+    content: ']';
+    position: absolute;
+    right: -30px;
+    line-height: 40px;
 }
 .content:after,
 .content:before {
-  color: #16a085;
-  font-size: 42px;
-  animation: 2s linear 0s normal none infinite opacity;
-  -webkit-animation: 2s ease-out 0s normal none infinite opacity;
-  -moz-animation: 2s ease-out 0s normal none infinite opacity;
-  -o-animation: 2s ease-out 0s normal none infinite opacity;
+    color: #16a085;
+    font-size: 42px;
+    animation: 2s linear 0s normal none infinite opacity;
+    -webkit-animation: 2s ease-out 0s normal none infinite opacity;
+    -moz-animation: 2s ease-out 0s normal none infinite opacity;
+    -o-animation: 2s ease-out 0s normal none infinite opacity;
 }
 .visible {
-  float: left;
-  font-weight: 600;
-  overflow: hidden;
-  height: 40px;
+    float: left;
+    font-weight: 600;
+    overflow: hidden;
+    height: 40px;
 }
 p {
-  display: inline;
-  float: left;
-  margin: 0;
+    display: inline;
+    float: left;
+    margin: 0;
 }
 ul {
-root@9ed70169161e:/var/www/html# cat index.php 
-<html>
-<header>
-<link href='http://fonts.googleapis.com/css?family=Muli' rel='stylesheet' type='text/css'>
-<style type="text/css" media="screen">
-body {
-  width: 100%;
-  height: 100%;
-  position: fixed;
-  background-color: #34495e;
-}
-.content {
-  width: 400px;
-  font-size: 36px;
-  line-height: 40px;
-  font-family: 'Muli';
-  color: #ecf0f1;
-  height: 40px;
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  margin-top: -15px;
-  margin-left: -118px;
-}
-.content:before {
-  content: '[';
-  position: absolute;
-  left: -30px;
-  line-height: 40px;
-}
-.content:after {
-  content: ']';
-  position: absolute;
-  right: -30px;
-  line-height: 40px;
-}
-.content:after,
-.content:before {
-  color: #16a085;
-  font-size: 42px;
-  animation: 2s linear 0s normal none infinite opacity;
-  -webkit-animation: 2s ease-out 0s normal none infinite opacity;
-  -moz-animation: 2s ease-out 0s normal none infinite opacity;
-  -o-animation: 2s ease-out 0s normal none infinite opacity;
-}
-.visible {
-  float: left;
-  font-weight: 600;
-  overflow: hidden;
-  height: 40px;
-}
-p {
-  display: inline;
-  float: left;
-  margin: 0;
-}
-ul {
-  margin-top: 0;
-  padding-left: 140px;
-  text-align: left;
-  list-style: none;
-  animation: 6s linear 0s normal none infinite change;
-  -webkit-animation: 6s linear 0s normal none infinite change;
-  -moz-animation: 6s linear 0s normal none infinite change;
-  -o-animation: 6s linear 0s normal none infinite change;
+    margin-top: 0;
+    padding-left: 250px;
+    text-align: left;
+    list-style: none;
+    animation: 6s linear 0s normal none infinite change;
+    -webkit-animation: 6s linear 0s normal none infinite change;
+    -moz-animation: 6s linear 0s normal none infinite change;
+    -o-animation: 6s linear 0s normal none infinite change;
 }
 ul li {
-  line-height: 40px;
-  margin: 0;
+    line-height: 40px;
+    margin: 0;
 }
 @-webkit-keyframes opacity {
-  0% {
-    opacity: 0;
-  }
-  50% {
-    opacity: 1;
-  }
-  100% {
-    opacity: 0;
-  }
+    0% {
+        opacity: 0;
+    }
+    50% {
+        opacity: 1;
+    }
+    100% {
+        opacity: 0;
+    }
 }
 @keyframes opacity {
-  0% {
-    opacity: 0;
-  }
-  50% {
-    opacity: 1;
-  }
-  100% {
-    opacity: 0;
-  }
+    0% {
+        opacity: 0;
+    }
+    50% {
+        opacity: 1;
+    }
+    100% {
+        opacity: 0;
+    }
 }
 @-webkit-keyframes change {
-  0% {
-    margin-top: 0;
-  }
-  15% {
-    margin-top: 0;
-  }
-  25% {
-    margin-top: -40px;
-  }
-  40% {
-    margin-top: -40px;
-  }
-  50% {
-    margin-top: -80px;
-  }
-  65% {
-    margin-top: -80px;
-  }
-  75% {
-    margin-top: -40px;
-  }
-  85% {
-    margin-top: -40px;
-  }
-  100% {
-    margin-top: 0;
-  }
+    0% {
+        margin-top: 0;
+    }
+    15% {
+        margin-top: 0;
+    }
+    25% {
+        margin-top: -40px;
+    }
+    40% {
+        margin-top: -40px;
+    }
+    50% {
+        margin-top: -80px;
+    }
+    65% {
+        margin-top: -80px;
+    }
+    75% {
+        margin-top: -40px;
+    }
+    85% {
+        margin-top: -40px;
+    }
+    100% {
+        margin-top: 0;
+    }
 }
 @keyframes change {
-  0% {
-    margin-top: 0;
-  }
-  15% {
-    margin-top: 0;
-  }
-  25% {
-    margin-top: -40px;
-  }
-  40% {
-    margin-top: -40px;
-  }
-  50% {
-    margin-top: -80px;
-  }
-  65% {
-    margin-top: -80px;
-  }
-  75% {
-    margin-top: -40px;
-  }
-  85% {
-    margin-top: -40px;
-  }
-  100% {
-    margin-top: 0;
-  }
+    0% {
+        margin-top: 0;
+    }
+    15% {
+        margin-top: 0;
+    }
+    25% {
+        margin-top: -40px;
+    }
+    40% {
+        margin-top: -40px;
+    }
+    50% {
+        margin-top: -80px;
+    }
+    65% {
+        margin-top: -80px;
+    }
+    75% {
+        margin-top: -40px;
+    }
+    85% {
+        margin-top: -40px;
+    }
+    100% {
+        margin-top: 0;
+    }
 }
-
 </style>
 </header>
 <body>
@@ -212,7 +156,7 @@ ul li {
   <div class='visible'>
     
     <p>
-	Bonjour 
+	PHP : Bonjour 
     </p>
 
     <ul>
