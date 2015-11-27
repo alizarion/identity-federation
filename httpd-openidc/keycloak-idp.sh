c937675930519198f521d209332e1448379cbbeb
alizarion <selim.bensenouci@gmail.com>
2015-11-27 17:12:27 +0100

    Delete keycloak-idp.sh

--- httpd-openidc/keycloak-idp.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-export OIDCProviderMetadataURL=http://keycloak.crossview.io/auth/realms/supplier-portal
-export OIDCClientID=supplier-portal-rest-api
-export OIDCClientSecret=ac080650-3773-4136-b54e-2a433418cbdd
-export http_proxy=
-export https_proxy=
-docker-compose up
