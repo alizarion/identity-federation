a6acf13f66af432fb05c831e315de8f1ff56d7db
alizarion <selim.bensenouci@gmail.com>
2015-12-08 10:46:00 +0100

    Update init_keycloak_schema.sql

--- keycloak/mysql/files/init_keycloak_schema.sql
+++ keycloak/mysql/files/init_keycloak_schema.sql
@@ -3,3 +3,8 @@ use keycloak;
 
 CREATE USER 'keycloak'@'%' IDENTIFIED BY 'keycloak';
 GRANT ALL PRIVILEGES ON *.* TO 'keycloak'@'%' WITH GRANT OPTION;
+
+CREATE USER 'keycloak'@'localhost' IDENTIFIED BY 'keycloak';
+GRANT ALL PRIVILEGES ON keycloak.* TO 'keycloak'@'localhost' WITH GRANT OPTION;
+FLUSH PRIVILEGES;
+commit;
