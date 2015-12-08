fe73058c0a9b0ff5d1d69636923ef8328158f58d
alizarion <selim.bensenouci@gmail.com>
2015-12-08 14:45:04 +0100

    Update init_keycloak_schema.sql

--- keycloak/mysql/files/init_keycloak_schema.sql
+++ keycloak/mysql/files/init_keycloak_schema.sql
@@ -1,5 +1,5 @@
-CREATE DATABASE keycloak;
 
+CREATE DATABASE keycloak DEFAULT CHARACTER SET utf8 DEFAULT COLLATE utf8_general_ci;
 
 CREATE USER 'keycloak'@'%' IDENTIFIED BY 'keycloak';
 GRANT ALL PRIVILEGES ON *.* TO 'keycloak'@'%' WITH GRANT OPTION;
