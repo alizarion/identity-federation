4919aa94e9034e4baa0c91871c53cacae7f6f6eb
alizarion <selim.bensenouci@gmail.com>
2015-12-08 10:20:22 +0100

    Update init_keycloak_schema.sql

--- keycloak/mysql/files/init_keycloak_schema.sql
+++ keycloak/mysql/files/init_keycloak_schema.sql
@@ -1,5 +1,5 @@
 create database keycloak;
 use keycloak;
 
-CREATE USER 'keycloak'@'%'IDENTIFIED BY 'keycloak';
+CREATE USER 'keycloak'@'%' IDENTIFIED BY 'keycloak';
 GRANT ALL PRIVILEGES ON *.* TO 'keycloak'@'%' WITH GRANT OPTION;
