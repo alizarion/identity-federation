854ca4efd619805eb9a6b58ebae9b902a212c21f
alizarion <selim@openlinux.fr>
2015-12-11 12:33:57 +0100

    adding saml support

---
+++ httpd-proxy-federate/entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e
+
+if [ -z "$OIDCProviderMetadataURL" -a -z "$OIDCClientID" -a -z "$OIDCClientSecret" -a -z "$OIDCRedirectURI" ]; then
+        echo >&1 'ERROR  Did you forget to add one of these -e OIDCProviderMetadataURL=... -e OIDCClientID=...-eOIDCClientSecret=.. -e OIDCRedirectURI=.. ?'
+        exit 1
+fi
+if [ -z "$SAMLMetadataURL" -a -z "$SAMLClientID" -a -z "$SAMLEntityID" ]; then
+        echo >&1 'ERROR  Did you forget to add one of these -e SAMLMetadataURL=... -e SAMLClientID=...-e SAMLEntityID=.. ?'
+        exit 1
+fi
+
+sed -e "s;%OIDCProviderMetadataURL%;$OIDCProviderMetadataURL;g" -e "s;%OIDCClientID%;$OIDCClientID;g" -e "s;%OIDCClientSecret%;$OIDCClientSecret;g" -e "s;%OIDCRedirectURI%;$OIDCRedirectURI;g" /tmp/mod_openid_conf.template > /etc/httpd/conf.d/openidc.conf
+sed -e "s;%SAMLMetadataURL%;$SAMLMetadataURL;g" -e "s;%SAMLClientID%;$SAMLClientID;g" -e "s;%SAMLEntityID%;$SAMLEntityID;g" /tmp/mod_shib_saml_conf.template > /etc/httpd/conf.d/shib_saml.conf
+
+exec "$@"
